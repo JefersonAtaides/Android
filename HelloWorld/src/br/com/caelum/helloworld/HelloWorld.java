a4e8968d47e94ce565fb6dc994df86607067a744
Anderson Dadario <anderson@dadario.com.br>
2013-06-29 14:57:29 -0300

    [FEATURE] TextView, Button, Dialog

--- HelloWorld/src/br/com/caelum/helloworld/HelloWorld.java
+++ HelloWorld/src/br/com/caelum/helloworld/HelloWorld.java
@@ -1,15 +1,72 @@
 package br.com.caelum.helloworld;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class HelloWorld extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("CICLO DE VIDA","onCreate");
         setContentView(R.layout.principal);
+        
+    	Button cadastrar = (Button) findViewById(R.id.botaoConfirmar);
+    	cadastrar.setOnClickListener(new View.OnClickListener(){
+    		public void onClick(View view){
+    			// pega os dados da Activity
+    			// (agora que temos acesso a todos os componentes, fica mais simples)
+    			// grava os dados na base SQL
+    		}
+    	});
+    	
+    	AlertDialog.Builder alerta = new AlertDialog.Builder(HelloWorld.this);
+    	alerta.setMessage("Olá amigo");
+    	alerta.setNeutralButton("OK", null);
+    	
+    	AlertDialog dialogo = alerta.create();
+    	dialogo.setTitle("Meu Diálogo");
+    	dialogo.show();
+    }
+    
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	Log.i("CICLO DE VIDA","onStart");
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	Log.i("CICLO DE VIDA","onResume");
+    }
+    
+    @Override
+    protected void onRestart() {
+    	super.onRestart();
+    	Log.i("CICLO DE VIDA","onRestart");
+    }
+    
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    	Log.i("CICLO DE VIDA","onPause");
+    }
+    
+    @Override
+    protected void onStop() {
+    	super.onStop();
+    	Log.i("CICLO DE VIDA","onStop");
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	super.onDestroy();
+    	Log.i("CICLO DE VIDA","onDestroy");
     }
 
     @Override
---
+++ HelloWorld/src/br/com/caelum/listeners/BotaoDeConfirmarListener.java
@@ -0,0 +1,10 @@
+package br.com.caelum.listeners;
+
+import android.view.View;
+
+public class BotaoDeConfirmarListener implements View.OnClickListener{
+	public void onClick(View view){
+		// pega os dados da Activity e
+		// grava os dados na base SQL
+	}
+}
